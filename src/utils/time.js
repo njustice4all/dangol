e075898b20cfa319ef217348f631a534ec6ef9f4
youngjoo <hyj@aty.kr>
2018-03-12 18:30:15 +0900

    iOS - android와 iOS의 시간처리 통합을 위한 moment설치 및 적용

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,4 +1,8 @@
+import moment from 'moment';
+
 export const getTime = date => {
-  const time = new Date(date);
-  return time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
+  const test = moment(date);
+  // const time = new Date(date);
+  // return time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
+  return test.hour() + ':' + test.minute();
 };
