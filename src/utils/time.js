665fe40296d962a85737712ec10a2e80046247f3
youngjoo <hyj@aty.kr>
2018-03-13 13:37:21 +0900

    시간 - moment-timezone 설치

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,8 +1,10 @@
 import moment from 'moment';
+import tz from 'moment-timezone';
 
 export const getTime = date => {
   const test = moment(date);
   // const time = new Date(date);
+
   // return time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
-  return test.hour() + ':' + test.minute();
+  return moment.tz(date, 'Asia/Seoul').format('H:mm');
 };
