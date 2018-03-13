1937d237341dba925fbc99b59c3e11b9214dfefd
youngjoo <hyj@aty.kr>
2018-03-13 13:41:29 +0900

    시간 - moment-timezone삭제

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,10 +1,11 @@
 import moment from 'moment';
-import tz from 'moment-timezone';
 
 export const getTime = date => {
   const test = moment(date);
+
+  return test.format('H:mm');
+
   // const time = new Date(date);
 
   // return time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
-  return moment.tz(date, 'Asia/Seoul').format('H:mm');
 };
